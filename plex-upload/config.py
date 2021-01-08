7eb825b75ba7f9001f576c2a24239ba0d56995f4
Fabian Volkers <fabian.volkers@gmail.com>
2021-01-08 13:30:34 +0100

    load config from application root

---
+++ plex-upload/config.py
@@ -0,0 +1,12 @@
+"""
+Settings File
+"""
+import os
+from get_docker_secret import get_docker_secret
+
+UPLOAD_FOLDER = os.path.abspath('/uploads')
+PLEX_MOVIE_FOLDER = os.path.abspath('/plex/movies')
+PLEX_TV_FOLDER = os.path.abspath('/plex/tv')
+PLEX_TV_URL = os.getenv('PLEX_TV_URL')
+PLEX_MOVIE_URL = os.getenv('PLEX_MOVIE_URL')
+SECRET_KEY = get_docker_secret('flask_secret_key', default='very_secret')
